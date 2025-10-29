13e22d599c07ad2164cf1caafb264beaf13a1fe6
Wanderley <davidgomeswanderley@gmail.com>
2025-10-28 21:37:11 -0300

    contando arquivos do node_modules

--- teste.js
+++ teste.js
@@ -1,3 +1,20 @@
-//console.log('Rodou no node');
+const fs = require('fs');
+const path = require('path');
 
-console.log('Angelo')
+function contarArquivos(diretorio) {
+  let total = 0;
+  const itens = fs.readdirSync(diretorio, { withFileTypes: true });
+
+  for (const item of itens) {
+    const caminho = path.join(diretorio, item.name);
+    if (item.isDirectory()) {
+      total += contarArquivos(caminho);
+    } else {
+      total++;
+    }
+  }
+  return total;
+}
+
+const totalArquivos = contarArquivos('./node_modules');
+console.log(`Total de arquivos no node_modules: ${totalArquivos}`);
